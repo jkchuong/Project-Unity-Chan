5cef4711a078adc5884ce484c25808d2bab5cb58
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2022-01-29 18:14:29 +0000

    Moving one obstacle and reseting position after max life time

--- Assets/Project/Scripts/MoveObstacle.cs
+++ Assets/Project/Scripts/MoveObstacle.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Project.Scripts;
 
 public class MoveObstacle : MonoBehaviour
 {
-    #region Components
-    private Rigidbody boxComponent;
+    #region OutsideComponents
+    private Rigidbody obstacle;
     #endregion
 
     #region Fields
@@ -15,23 +16,35 @@ public class MoveObstacle : MonoBehaviour
     public float objectSpeed = 8.0f;
     #endregion
 
+    #region Object Pooling fields
+    [SerializeField] private Transform obstacleOriginLocation;
+    [SerializeField] private float maxLifetime = 5; //  5s 
+    private float lifetime; //  5s 
+    private ObjectPool _objectPoolingService;
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
-        boxComponent = GetComponent<Rigidbody>();
+        obstacle = GetComponent<Rigidbody>();        
+        direction = new Vector3(0, 0, -1); // default
 
-        // default
-        direction = new Vector3(0, 0, -1);
+        // object pooling
+        //_objectPoolingService = new ObjectPool();
+        //var x = _objectPoolingService.Bag["Cube"];
+        //x.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
-    {        
-        MoveObject(direction, objectSpeed);
+    {
+        Move(direction, objectSpeed);
+        CheckObstacleLifeTime();
     }
 
     //Move object at specific direction and speed
-    private void MoveObject(Vector3 direction_, float speed)
+    private void Move(Vector3 direction_, float speed)
     {
         velocity = direction_;
         velocity = transform.TransformDirection(velocity);
@@ -39,5 +52,24 @@ public class MoveObstacle : MonoBehaviour
 
         // move object
         transform.localPosition += velocity * Time.deltaTime;
+    }
+
+    // object pooling driver code
+    private void CheckObstacleLifeTime()
+    {
+        lifetime += Time.deltaTime;
+        if (lifetime > maxLifetime) 
+        {
+            gameObject.SetActive(false);
+            ResetObstacle();
+        }
+
+    }
+    // set as inactive and reset position
+    private void ResetObstacle()
+    {        
+        lifetime = 0;
+        gameObject.SetActive(true);
+        gameObject.transform.position = obstacleOriginLocation.position;
     }
 }
