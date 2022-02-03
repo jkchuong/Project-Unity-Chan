35fbf895459c19023e08a0bb779542a1c6003e5c
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2022-02-03 22:37:57 +0000

    refactoring of large methods: update(), fixedupdate() and making dictionary action for states

--- Assets/Project/Scripts/UnityChanControlScript.cs
+++ Assets/Project/Scripts/UnityChanControlScript.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 
 namespace UnityChan
 {
@@ -9,20 +9,13 @@ namespace UnityChan
     [RequireComponent(typeof(Rigidbody))]
     public class UnityChanControlScript : MonoBehaviour
     {
-        public float animSpeed = 1.5f;
+        #region Fields
+        //        
         public float lookSmoother = 3.0f;
         public bool useCurves = true;
-
         public float useCurvesHeight = 0.5f;
 
-        public float forwardSpeed = 7.0f;
-
-        public float backwardSpeed = 0.0f;
-
-        public float rotateSpeed = 2.0f;
-
-        public float jumpPower = 3.0f;
-
+        //
         private CapsuleCollider col;
         private Rigidbody rb;
         private Vector3 velocity;
@@ -31,33 +24,65 @@ namespace UnityChan
         private Animator anim;
         private AnimatorStateInfo currentBaseState;
 
+        //
         private float horizontalInput;
         private float verticalInput = 1;
+        public float leftRightSpeed = 7.0f;
+        public float rotateSpeed = 2.0f;
+        public float jumpPower = 5.0f;
+        public float animSpeed = 1.5f;
 
-        static int idleState = Animator.StringToHash("Base Layer.Idle");
-        static int locoState = Animator.StringToHash("Base Layer.Locomotion");
-        static int jumpState = Animator.StringToHash("Base Layer.Jump");
-        static int restState = Animator.StringToHash("Base Layer.Rest");
+        //
+        private int idleState = Animator.StringToHash("Base Layer.Idle");
+        private int locoState = Animator.StringToHash("Base Layer.Locomotion");
+        private int jumpState = Animator.StringToHash("Base Layer.Jump");
+        private int restState = Animator.StringToHash("Base Layer.Rest");
+
+        Dictionary<int, Action> animationStates;
+        #endregion
 
         private void Start()
         {
             anim = GetComponent<Animator>();
-
             col = GetComponent<CapsuleCollider>();
             rb = GetComponent<Rigidbody>();
 
             orgColHight = col.height;
             orgVectColCenter = col.center;
+
+            // populate dictionary
+            animationStates = new Dictionary<int, Action>();
+            animationStates.Add(idleState, () => WhenIdle());
+            animationStates.Add(locoState, () => WhenRunning());
+            animationStates.Add(jumpState, () => WhenJumping());
+            animationStates.Add(restState, () => WhenResting());
         }
 
         private void Update()
         {
+            SetAnimations();
+            JumpControls();
+        }
+
+        private void FixedUpdate()
+        {
+            SidewaysMovement();
+
+            // perform action depending on current state
+            currentBaseState = anim.GetCurrentAnimatorStateInfo(0);            ;
+            ActionsDependingOnAnimationState(currentBaseState.nameHash);
+        }
+
+        #region Functions for Update()
+        private void SetAnimations()
+        {
             horizontalInput = Input.GetAxis("Horizontal");
-            // verticalInput = Input.GetAxis("Vertical");
             anim.SetFloat("Speed", verticalInput);
             anim.SetFloat("Direction", horizontalInput);
             anim.speed = animSpeed;
-            currentBaseState = anim.GetCurrentAnimatorStateInfo(0);
+        }
+        private void JumpControls()
+        {
             rb.useGravity = true;
 
             if (Input.GetButtonDown("Jump"))
@@ -65,96 +90,96 @@ namespace UnityChan
                 rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
                 anim.SetBool("Jump", true);
             }
-           
         }
+        #endregion
 
-        private void FixedUpdate()
+        #region Functions for FixedUpdate()
+
+        // enables player to only move move sideways
+        private void SidewaysMovement()
         {
-            
             velocity = new Vector3(horizontalInput, 0, 0);
-
             velocity = transform.TransformDirection(velocity);
-
-            if (horizontalInput > 0.1)
-            {
-                velocity *= forwardSpeed;
-            }
-            else if (horizontalInput < -0.1)
-            {
-                velocity *= backwardSpeed;
-            }
-
+            velocity *= leftRightSpeed;
             transform.localPosition += velocity * Time.fixedDeltaTime;
-
-            transform.Rotate(0, 0, 0);
-
-            if (currentBaseState.nameHash == locoState)
-            {
-                if (useCurves)
-                {
-                    resetCollider();
-                }
-            }
-
-            else if (currentBaseState.nameHash == jumpState)
-            {
-                if (!anim.IsInTransition(0))
-                {
-                    if (useCurves)
-                    {
-                        float jumpHeight = anim.GetFloat("JumpHeight");
-                        float gravityControl = anim.GetFloat("GravityControl");
-                        if (gravityControl > 0)
-                            rb.useGravity = false;
-
-                        Ray ray = new Ray(transform.position + Vector3.up, -Vector3.up);
-                        RaycastHit hitInfo = new RaycastHit();
-
-                        if (Physics.Raycast(ray, out hitInfo))
-                        {
-                            if (hitInfo.distance > useCurvesHeight)
-                            {
-                                col.height = orgColHight - jumpHeight;
-                                float adjCenterY = orgVectColCenter.y + jumpHeight;
-                                col.center = new Vector3(0, adjCenterY, 0);
-                            }
-                            else
-                            {
-                                resetCollider();
-                            }
-                        }
-                    }
-
-                    anim.SetBool("Jump", false);
-                }
-            }
-
-            else if (currentBaseState.nameHash == idleState)
-            {
-                if (useCurves)
-                {
-                    resetCollider();
-                }
-
-                if (Input.GetButtonDown("Jump"))
-                {
-                    anim.SetBool("Rest", true);
-                }
-            }
-
-            else if (currentBaseState.nameHash == restState)
-            {
-                if (!anim.IsInTransition(0))
-                {
-                    anim.SetBool("Rest", false);
-                }
-            }
         }
 
-        void resetCollider()
+        // performs action depending on current state of character
+        private void ActionsDependingOnAnimationState(int currentState)
+        {            
+            if (animationStates.ContainsKey(currentState))
+            {
+                animationStates[currentState](); // call action in dictionary that corresponds to currentState
+            }
+            return;
+        }
+        void ResetCollider()
         {
             col.height = orgColHight;
             col.center = orgVectColCenter;
         }
+
+        #region Actions depending on current state
+        void WhenRunning()
+        {
+            if (useCurves)
+            {
+                ResetCollider();
+            }
+        }
+        void WhenIdle()
+        {
+            if (useCurves)
+            {
+                ResetCollider();
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetBool("Rest", true);
+            }
+        }
+        void WhenJumping()
+        {
+            if (!anim.IsInTransition(0))
+            {
+                if (useCurves)
+                {
+                    float jumpHeight = anim.GetFloat("JumpHeight");
+                    float gravityControl = anim.GetFloat("GravityControl");
+                    if (gravityControl > 0)
+                        rb.useGravity = false;
+
+                    Ray ray = new Ray(transform.position + Vector3.up, -Vector3.up);
+                    RaycastHit hitInfo = new RaycastHit();
+
+                    if (Physics.Raycast(ray, out hitInfo))
+                    {
+                        if (hitInfo.distance > useCurvesHeight)
+                        {
+                            col.height = orgColHight - jumpHeight;
+                            float adjCenterY = orgVectColCenter.y + jumpHeight;
+                            col.center = new Vector3(0, adjCenterY, 0);
+                        }
+                        else
+                        {
+                            ResetCollider();
+                        }
+                    }
+                }
+
+                anim.SetBool("Jump", false);
+            }
+        }
+        void WhenResting()
+        {
+            if (!anim.IsInTransition(0))
+            {
+                anim.SetBool("Rest", false);
+            }
+        }
+        #endregion
+
+        #endregion
     }
 }
