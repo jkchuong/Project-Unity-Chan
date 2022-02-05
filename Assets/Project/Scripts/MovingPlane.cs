44f3aaa7920afb7eee753af8b12623f6057b7eb7
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2022-02-05 17:28:03 +0000

    Making floor move

---
+++ Assets/Project/Scripts/MovingPlane.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlane : MonoBehaviour
+{
+
+    [SerializeField] private Transform planeOriginLocation;
+    [SerializeField] private Transform planeEndLocation;
+    [SerializeField] private float speed;
+    private Vector3 velocity;
+    private Vector3 direction;    
+
+    public void ActivateThis() => gameObject.SetActive(true);
+    public void DeactivateThis() => gameObject.SetActive(false);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = transform.forward * -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlaneTowardsPlayer();
+        ResetPositionWhenReaching(planeEndLocation.position);
+    }
+
+    private void MovePlaneTowardsPlayer()
+    {
+        velocity = direction;
+        velocity = transform.TransformDirection(velocity);
+        velocity *= speed;
+        transform.localPosition += velocity * Time.deltaTime;
+    }
+    private void ResetPositionWhenReaching(Vector3 endLocationPosition)
+    {
+        if (gameObject.transform.position.z <= endLocationPosition.z)
+        {
+            #if UNITY_EDITOR
+                Debug.LogWarning($"{gameObject.name}: {gameObject.transform.position} vs {endLocationPosition}");
+            #endif
+
+            DeactivateThis();
+            gameObject.transform.position = planeOriginLocation.position;
+            ActivateThis();
+        }
+    }    
+}
