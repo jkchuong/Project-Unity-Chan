4a6840eb1499e862a6530999caee62a5bb9b3562
Jimmy <jkchuong@gmail.com>
2022-02-09 23:09:10 +0000

    Removed unnescescary blend files and removed debug from obstacle

--- Assets/Project/Scripts/ObstacleRemover.cs
+++ Assets/Project/Scripts/ObstacleRemover.cs
@@ -9,9 +9,7 @@ public class ObstacleRemover : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Obstacle obstacle = other.GetComponent<Obstacle>();
-
-        Debug.Log($"{obstacle.name}");
-
+        
         if (obstacle)
         {
             obstacle.gameObject.SetActive(false);
